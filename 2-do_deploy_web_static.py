e1dcb29ae847a14edf101f7b53c5e6e47afeeca8
root <sranciato1@gmail.com>
2020-01-04 23:19:06 +0000

    [ADD] if file doesnt exist return false

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,6 +2,7 @@
 """generates a .tgz archive from the contents of the web_static folder"""
 from fabric.operations import *
 from datetime import datetime
+import os
 
 
 def do_pack():
@@ -14,3 +15,12 @@ def do_pack():
         return "versions/web_static_{}.tgz".format(time)
     except Exception:
         return None
+
+
+def do_deploy(archive_path):
+    """distributes an archive to your web servers"""
+    if (os.path.exists(archive_path)):
+        path = archive_path.split('/')[1]
+        return True
+    else:
+        return False
