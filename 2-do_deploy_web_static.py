c1f6d0cbaa5a7775f2193b17d73b94f7e3a4843c
root <sranciato1@gmail.com>
2020-01-05 20:16:08 +0000

    [ADD] deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """generates a .tgz archive from the contents of the web_static folder"""
-from fabric.operations import *
+from fabric.api import local, env, put, run
 from datetime import datetime
 import os
+env.hosts = ['35.196.153.56', '34.73.85.88']
+env.user = "ubuntu"
 
 
 def do_pack():
@@ -20,7 +22,20 @@ def do_pack():
 def do_deploy(archive_path):
     """distributes an archive to your web servers"""
     if (os.path.exists(archive_path)):
-        path = archive_path.split('/')[1]
-        return True
+        try:
+            path = archive_path.split('/')[1]
+            no_ext = path.split('.')[0]
+            data = "/data/web_static/releases/" + no_ext + "/"
+            put(archive_path, "/tmp/")
+            run("mkdir -p {}".format(data))
+            run("tar -xzf /tmp/{} -C {}".format(path, data))
+            run("rm /tmp/{}".format(path))
+            run("mv {}web_static/* {}".format(data, data))
+            run("rm -rf {}web_static".format(data))
+            run("rm -rf /data/web_static/current")
+            run("ln -s {} /data/web_static/current".format(data))
+            return True
+        except Exception:
+            return False
     else:
         return False
