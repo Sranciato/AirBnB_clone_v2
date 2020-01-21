23e4d13145cb98e0c310ce9d7cbddb01cbf8dfc9
sranciato <sranciato1@gmail.com>
2020-01-21 12:07:22 +0000

    make hbnb route display HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from flask import Flask
 
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -10,6 +9,13 @@ app.url_map.strict_slashes = False
 def start_server():
     """start server and display hello hbnb"""
     return ("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def display_hbnb():
+    """HBNB route must return HBNB"""
+    return ("HBNB")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
